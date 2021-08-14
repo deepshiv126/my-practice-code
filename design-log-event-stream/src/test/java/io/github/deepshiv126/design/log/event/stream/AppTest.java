caefc72cac5a022b3ebeca039825eefda3d103f4
DShiv <12883918+deepshiv126@users.noreply.github.com>
2021-08-13 23:30:49 -0700

    Minor Fixes

--- design-log-event-stream/src/test/java/io/github/deepshiv126/design/log/event/stream/AppTest.java
+++ design-log-event-stream/src/test/java/io/github/deepshiv126/design/log/event/stream/AppTest.java
@@ -7,12 +7,11 @@ import org.testng.annotations.Test;
 public class AppTest {
 
     @Test
-    public void main() {
+    public void appTest() {
+        final int windowSize = 300;
 
         final String inputFile = this.getClass().getResource("/test-input-file.log").getFile();
         final String outputFile = this.getClass().getResource("/test-output-file.log").getFile();
-
-        final int windowSize = 300;
 
         final BufferLogEventStream bufferLogEventStream = new BufferLogEventStream();
         bufferLogEventStream.bufferLogEvents(inputFile, outputFile, windowSize);
