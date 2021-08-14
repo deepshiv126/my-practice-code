ddcac1c6e9e10acf796352579d96c9c5a8ac0e6e
DShiv <12883918+deepshiv126@users.noreply.github.com>
2021-08-13 23:35:19 -0700

    Minor Fixes

--- design-log-event-stream/src/test/java/io/github/deepshiv126/design/log/event/stream/AppTest.java
+++ design-log-event-stream/src/test/java/io/github/deepshiv126/design/log/event/stream/AppTest.java
@@ -10,8 +10,8 @@ public class AppTest {
     public void appTest() {
         final int windowSize = 300;
 
-        final String inputFile = this.getClass().getResource("/test-input-file.log").getFile();
-        final String outputFile = this.getClass().getResource("/test-output-file.log").getFile();
+        final String inputFile = getClass().getClassLoader().getResource("test-input-file.log").getFile();
+        final String outputFile = getClass().getClassLoader().getResource("test-output-file.log").getFile();
 
         final BufferLogEventStream bufferLogEventStream = new BufferLogEventStream();
         bufferLogEventStream.bufferLogEvents(inputFile, outputFile, windowSize);
